7df28cb040889653019559546d533a5792671d94
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-05-05 11:31:57 +0300

    Initial Commit

---
+++ Swift_project_training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_project_training
+//
+//  Created by MacBook Pro on 05.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
