ac9e67c087f5be0b167ba63764f388db70ba3cc9
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-05-05 12:01:59 +0300

    deleteComments

--- Swift_project_training/ViewController.swift
+++ Swift_project_training/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  Swift_project_training
-//
-//  Created by MacBook Pro on 05.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
